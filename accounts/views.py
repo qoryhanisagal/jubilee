55a780006e76bb6cd693f1ed553a22f4cef6b87e
Sequoyah Descoteaux <sequoyah.m.dozier@gmail.com>
2025-04-28 22:38:26 -0500

    Add register view with auto-login and success message

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
+from django.contrib import messages
 
-# Create your views here.
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user) # Aut-login after registration
+            
+            messages.success(request, "Your account was created successfully! Welcome to Jubilee.") # Send a success message
+
+            return redirect('home')  # After successful registeration, redirect to home
+    else:
+        form = UserCreationForm()
+    return render(request, 'registration/register.html', {'form': form}) # This is the registration view
