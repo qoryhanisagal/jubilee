43793bdfdf27988f6cd0d0c762e9311e4fbeac9b
Sequoyah Descoteaux <sequoyah.m.dozier@gmail.com>
2025-04-30 22:15:37 -0500

    Update settings to include password change templates and improved navigation

--- config/settings.py
+++ config/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'posts', # Custom app for managing posts    
 ]
 
 MIDDLEWARE = [
