ed7283402a173043912ec63ee51c8ea5ae34f3d6
Sequoyah Descoteaux <sequoyah.m.dozier@gmail.com>
2025-04-30 20:43:58 -0500

    Update accounts URLs to include signup route using SignUpView

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from . import views
+from .views import SignUpView
 
 urlpatterns = [
-    path('register/', views.register, name='register'),
+    path('register/', SignUpView.as_view(), name='signup'),
 ]
