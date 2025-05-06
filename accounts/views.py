a749ae18053832eb3514c82134a5789dfea93035
Sequoyah Descoteaux <sequoyah.m.dozier@gmail.com>
2025-05-05 21:46:38 -0500

    Refactor accounts views for improved structure or auth handling

--- accounts/views.py
+++ accounts/views.py
@@ -1,10 +1,18 @@
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.views import PasswordChangeView
 from django.views.generic import CreateView
 from django.urls import reverse_lazy
 
-
-# Class-Based View for Registration (using reverse_lazy to redirect to login page)
+# Registration view
 class SignUpView(CreateView):
     form_class = UserCreationForm
     template_name = 'registration/register.html'
-    success_url = reverse_lazy('login')  # Redirect to login page after successful registration
+    success_url = reverse_lazy('login')  # After successful registration
+
+# Custom Password Change view
+class CustomPasswordChangeView(PasswordChangeView):
+    template_name = 'registration/password_change_form.html'
+    
+    # Redirect to the homepage after password change
+    def get_success_url(self):
+        return reverse_lazy('home')  # Redirect to the homepage after password change
