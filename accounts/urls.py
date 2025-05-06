c94494f8d89286b84e92afb88b91474a271f32d7
Sequoyah Descoteaux <sequoyah.m.dozier@gmail.com>
2025-05-05 21:38:23 -0500

    Refactor accounts views and include SignUpView

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from .views import SignUpView
+from .views import SignUpView, CustomPasswordChangeView
+from django.contrib.auth.views import PasswordChangeDoneView
 
 urlpatterns = [
     path('register/', SignUpView.as_view(), name='signup'),
+    path('password_change/', CustomPasswordChangeView.as_view(), name='password_change'),
+    path('password_change/done/', PasswordChangeDoneView.as_view(template_name='registration/password_change_done.html'), name='password_change_done'),
 ]
