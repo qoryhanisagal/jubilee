4fa586a43bc1f0075320eccf86f6e6a8c79b37cf
Sequoyah Descoteaux <sequoyah.m.dozier@gmail.com>
2025-05-07 21:24:24 -0500

    Add URL patterns for Draft and Archived post views

--- posts/urls.py
+++ posts/urls.py
@@ -5,16 +5,20 @@ from .views import (
     PostCreateView,
     PostUpdateView,
     PostDeleteView,
-    Test_403_View,
+    test_403_view,
+    DraftPostListView,  
+    ArchivedPostListView
 )
 
 app_name = 'posts'
 
 urlpatterns = [
     path('', PostListView.as_view(), name='post_list'),
+    path('drafts/', DraftPostListView.as_view(), name='post_drafts'), 
+    path('archived/', ArchivedPostListView.as_view(), name='post_archived'),
     path('<int:pk>/', PostDetailView.as_view(), name='post_detail'),
     path('create/', PostCreateView.as_view(), name='post_create'),
     path('<int:pk>/edit/', PostUpdateView.as_view(), name='post_edit'),
     path('<int:pk>/delete/', PostDeleteView.as_view(), name='post_delete'),
-    path('forbidden/', Test_403_View, name='forbidden'),
+    path('forbidden/', test_403_view, name='forbidden'),    
 ]
